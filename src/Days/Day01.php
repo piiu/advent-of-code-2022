faf08385dc2c3f87d956de5c610964718e75e0a5
piiu.pilt <piiu.pilt@helmes.com>
2022-12-01 14:36:39 +0200

    Simplify Day 1

--- src/Days/Day01.php
+++ src/Days/Day01.php
@@ -8,16 +8,9 @@ class Day01 extends BaseDay
 {
     public function execute()
     {
-        $food = $this->getInputArray();
-        $elves = [];
-        $sum = 0;
-        foreach ($food as $calories) {
-            if (empty(trim($calories))) {
-                $elves[] = $sum;
-                $sum = 0;
-            }
-            $sum += (int)$calories;
-        }
+        $elves = array_map(function($elf) {
+            return array_sum(explode("\n", $elf));
+        }, $this->getInputArray("\n\r\n"));
         rsort($elves);
         $this->part1 = $elves[0];
         $this->part2 = array_sum(array_splice($elves, 0, 3));
