94cbe74d72704a9a149d7fbdcd6434e9b93acf69
piiu.pilt <piiu.pilt@helmes.com>
2022-12-11 11:49:51 +0200

    Optimise day 9

---
+++ src/Days/Day09/Knot.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AdventOfCode\Days\Day09;
+
+use AdventOfCode\Common\Coordinates\Location;
+
+class Knot
+{
+    private Location $location;
+    private ?self $nextKnot;
+    public array $history = [];
+
+    public function __construct(self $nextKnot = null)
+    {
+        $this->nextKnot = $nextKnot;
+        $this->location = new Location();
+        $this->writeHistory();
+    }
+
+    public function move(array $directions)
+    {
+        foreach ($directions as $direction) {
+            $this->location->move($direction);
+        }
+        $this->writeHistory();
+
+        if (!$this->nextKnot) {
+            return;
+        }
+
+        $xOffset = $this->location->x - $this->nextKnot->location->x;
+        $yOffset = $this->location->y - $this->nextKnot->location->y;
+        if (abs($xOffset) > 1 || abs($yOffset) > 1) {
+            $directions = [];
+            if ($xOffset !== 0) {
+                $directions[] = $xOffset > 0 ? Location::RIGHT : Location::LEFT;
+            }
+            if ($yOffset !== 0) {
+                $directions[] = $yOffset > 0 ? Location::DOWN : Location::UP;
+            }
+            $this->nextKnot->move($directions);
+        }
+    }
+
+    public function writeHistory()
+    {
+        $this->history[] = $this->location->x . '|' . $this->location->y;
+    }
+
+    public function getLastKnotHistory() : array
+    {
+        return $this->nextKnot ? $this->nextKnot->getLastKnotHistory() : $this->history;
+    }
+
+    public function getNextKnot(): ?Knot
+    {
+        return $this->nextKnot;
+    }
+}
--- src/Days/Day09/Solution.php
+++ src/Days/Day09/Solution.php
@@ -8,26 +8,19 @@ class Solution extends BaseDay
 {
     public function execute()
     {
-        $rope1 = $this->getRope(1);
-        $rope2 = $this->getRope(9);
+        $head = new Knot();
+        for ($i = 0; $i < 9; $i++) {
+            $head = new Knot($head);
+        }
 
         foreach ($this->getInputArray() as $move) {
             [$direction, $amount] = explode(' ', $move);
             for ($i = 0; $i < $amount; $i++) {
-                $rope1->move([$direction]);
-                $rope2->move([$direction]);
+                $head->move([$direction]);
             }
         }
-        $this->part1 = count(array_unique($rope1->getTailHistory()));
-        $this->part2 = count(array_unique($rope2->getTailHistory()));
-    }
 
-    private function getRope(int $tailLength) : RopeSegment
-    {
-        $rope = new RopeSegment();
-        for ($i = 0; $i < $tailLength; $i++) {
-            $rope = new RopeSegment($rope);
-        }
-        return $rope;
+        $this->part1 = count(array_unique($head->getNextKnot()->history));
+        $this->part2 = count(array_unique($head->getLastKnotHistory()));
     }
 }
