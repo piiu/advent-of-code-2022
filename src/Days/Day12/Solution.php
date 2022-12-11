029ad39587c748438a2ba25e8be282d197547553
piiu.pilt <piiu.pilt@helmes.com>
2022-12-11 14:47:13 +0200

    Simplify and clean up day 11

---
+++ src/Days/Day12/Solution.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AdventOfCode\Days\Day12;
+
+use AdventOfCode\Common\BaseDay;
+use AdventOfCode\Common\Console\Utils;
+
+class Solution extends BaseDay
+{
+    public function execute()
+    {
+
+    }
+}
